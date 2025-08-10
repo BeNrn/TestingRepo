d23463b6e98d2003a4978203af78cf058726a7a7
ben <rnnbe3@outlook.com>
2025-08-10 22:31:11 +0200

    Add Data input, save and id column

--- Python/OpenSourceDatabase_Dashboard.py
+++ Python/OpenSourceDatabase_Dashboard.py
@@ -3,6 +3,7 @@
 Purpose:    Script for an open source database.
 Created:    25.07.2025
 Author:     BeNrn
+Updated:    10.08.2025
 """
 #https://realpython.com/python-dash/#style-your-dash-application
 #https://dash.plotly.com/dash-core-components/tabs (for the tabs)
@@ -17,7 +18,7 @@ Author:     BeNrn
 import pandas as pd
 import os
 
-from dash import Dash, Input, Output, dcc, html, dash_table, callback
+from dash import Dash, Input, Output, dcc, html, dash_table, callback, ALL
 
 #load paths
 user_name = os.getlogin()
@@ -26,7 +27,7 @@ base_path = r"C:\Users\{}\Documents\Freizeit\Geschenke\2025_Toms_Pflanzendatenba
 database_path = os.path.join(base_path, "openDB.csv")
 
 #load the data
-db_df = pd.read_csv(database_path, sep = ";")
+db_df = pd.read_csv(database_path, sep = ";", dtype = {"Bild_ID" : "str"}, index_col=[0])
 
 #laod matching images (image name starts with Bild_ID)
 image_list = os.listdir(os.path.join(base_path, "assets"))
@@ -40,8 +41,16 @@ image_name_list = [f'<img src="/assets/{i}" height="60">' for i in image_name_li
 
 db_df["Bild_path"] = image_name_list
 
+list_of_dict = db_df.to_dict('records')
+for index, element in enumerate(list_of_dict):
+    element["ID"] = db_df.index[index]
+
 #start a dash instance
-app = Dash(__name__)
+# suppress_callback_exceptions=True -> the line "html.Button('Speichern', id='save_button')"
+# is only in the second tab, however, dash expect it to be in the initial setup
+# which would lead to the creation of the save button in every tab. The option
+# allows elements to not be loaded immediately
+app = Dash(__name__, suppress_callback_exceptions=True)
 
 #set the style of the webpage
 #html.Div -> create a new division
@@ -57,10 +66,25 @@ app.layout = html.Div([
     ])
 
 #controls the tabs, based on user input
-@callback(Output("database", 'children'),
-              Input('tabs', 'value'))
+@callback(
+        Output("database", 'children'),
+        Input('tabs', 'value'))
 
 def render_content(tab):
+    """
+    Create the layout
+
+    Parameters
+    ----------
+    tab : string
+        The tab, the user clicks on.
+
+    Returns
+    -------
+    None.
+
+    """
+    #tab 1
     if tab == 'tab_table':
         return html.Div([
             html.H1(children="Pflanzentabelle"),
@@ -72,112 +96,59 @@ def render_content(tab):
                 ),
             ),
             #https://dash.plotly.com/datatable
+            
             dash_table.DataTable( 
-                data = db_df.to_dict('records'),
-                columns = [{'name': col, 'id': col, 'presentation': 'markdown'} for col in db_df.columns],
+                data = list_of_dict,
+                #add id column manually
+                columns = [{'name': 'ID', 'id': 'ID', 'presentation': 'markdown'}] + [{'name': col, 'id': col, 'presentation': 'markdown'} for col in db_df.columns],
                 style_table = {'overflowX': 'auto'},
                 style_cell = {'textAlign': 'left'},
                 style_data={'whiteSpace': 'normal', 'height': 'auto'},
                 markdown_options={'html': True}
             )
         ])
+    #tab 2
     elif tab == 'tab_datainput':
         return html.Div([
             html.H3("Dateneingabe"),
-            #list(db_df) -> hier für jede Überschrift einen Block einfügen
-            dcc.Input(id='input-name', type='text', placeholder='Name'),
-        ])
+            #list comprehension for each column in the df to dynamically depict 
+            # df input fields
+            html.Div([
+            #on inputs: https://dash.plotly.com/dash-core-components/input
+            dcc.Input(id={"type" : "dynamic_input", "index" : df_col}, 
+                          type = "text", 
+                          placeholder = f"{df_col}",
+                          value = None)
+                for df_col in db_df.columns if df_col != "ID"
+            ]),
+            #save button
+            html.Button('Speichern', id='save_button'),
+            html.Div(id = 'output')
+        ]) 
+
+#process input on the second tab
+@callback(
+    Output('output', 'children'),
+    Input({'type': 'dynamic_input', 'index': ALL}, 'value'),
+    Input('save_button', 'n_clicks')
+    )
+
+# def display_inputs(values, n_clicks):
+#     # values is a list of input values in the order of inputs generated
+#     return f"You entered: {values}, {n_clicks}"
+
+def save_data(values, n_clicks):
+    if n_clicks == None:
+        return "Bitte Daten eingeben."
+    #field "Bild_ID" must not be empty
+    elif values[db_df.columns.get_loc("Bild_ID")] is None:
+        return 'Das Feld "Bild_ID" darf nicht leers sein.'
+    elif values[db_df.columns.get_loc("Bild_ID")] is not None:
+        #list to series and append at the end
+        db_df.loc[len(db_df)] = values
+        db_df.to_csv(database_path, index = "ID", sep = ";")
+        return "Daten gespeichert!"
 
 #run the app
 if __name__ == "__main__":
     app.run(debug=True)
-    
-    
-    
-    
-#Dateneingabe in Dash: 
-# - Input-Komponenten wie `dcc.Input`, `dcc.Dropdown` oder `dash_table.DataTable` (editierbar!)
-
-
-# ## ✅ **Variante 1: Dateneingabe mit `dcc.Input` Feldern**
-
-# Benutzer gibt Daten ein, klickt auf einen Button, und die Eingabe wird zu einer Tabelle hinzugefügt.
-
-# ### 🧪 Beispiel:
-
-# ```python
-import dash
-from dash import dcc, html, Input, Output, State
-import dash_table
-import pandas as pd
-
-# Start-Daten
-df = pd.DataFrame(columns=["Name", "Alter", "Beruf"])
-
-# App initialisieren
-app = dash.Dash(__name__)
-
-app.layout = html.Div([
-    html.H2("Person hinzufügen"),
-
-    # Eingabefelder
-    dcc.Input(id='input-name', type='text', placeholder='Name'),
-    dcc.Input(id='input-age', type='number', placeholder='Alter'),
-    dcc.Input(id='input-job', type='text', placeholder='Beruf'),
-    html.Button('Hinzufügen', id='add-button', n_clicks=0),
-    
-    html.Hr(),
-
-    # Tabelle zur Anzeige
-    dash_table.DataTable(
-        id='data-table',
-        columns=[{'name': col, 'id': col} for col in df.columns],
-        data=df.to_dict('records')
-    )
-])
-
-# Callback zum Hinzufügen von Daten
-@app.callback(
-    Output('data-table', 'data'),
-    Input('add-button', 'n_clicks'),
-    State('input-name', 'value'),
-    State('input-age', 'value'),
-    State('input-job', 'value'),
-    State('data-table', 'data'),
-    prevent_initial_call=True
-)
-def add_row(n_clicks, name, age, job, current_data):
-    if name and age and job:
-        current_data.append({'Name': name, 'Alter': age, 'Beruf': job})
-    return current_data
-
-if __name__ == '__main__':
-    app.run_server(debug=True)
-# ```
-
-# ---
-
-# ## ✅ **Variante 2: Direkt editierbare `DataTable`**
-
-# Du kannst die `DataTable` direkt **editierbar** machen:
-
-# ```python
-# dash_table.DataTable(
-#     id='editable-table',
-#     columns=[{'name': col, 'id': col, 'editable': True} for col in df.columns],
-#     data=df.to_dict('records'),
-#     editable=True
-# )
-# ```
-
-# → Änderungen werden direkt in der Tabelle gemacht. Du kannst mit einem Callback auch speichern, verarbeiten oder validieren, wenn sich die Daten ändern.
-
-# ---
-
-# ## ✅ Was du noch hinzufügen könntest:
-
-# * Validierung der Eingaben (z. B. kein leeres Feld zulassen)
-# * Zurücksetzen der Eingabefelder nach dem Absenden
-# * Speicherung in einer Datei oder Datenbank
-
-# Möchtest du eins davon auch sehen?
