ad8deedb979b2f3edfabfb55af22758b4608083f
ben <rnnbe3@outlook.com>
2025-08-19 21:50:17 +0200

    Style the dash

--- Python/OpenSourceDatabase_Dashboard.py
+++ Python/OpenSourceDatabase_Dashboard.py
@@ -57,13 +57,13 @@ def loadTable(database_path):
 
     #to string        
     #image_name_list = [f"{i}" for i in image_name_list]
-    image_name_list = [f'<img src="/assets/{i}" height="60">' if i is not None else None for i in image_name_list]
+    image_name_list = [f'<img src="/assets/{i}" height="100">' if i is not None else None for i in image_name_list]
 
     db_df["Bild_Name"] = image_name_list
 
-    list_of_dict = db_df.to_dict('records')
+    list_of_dict = db_df.to_dict("records")
     for index, element in enumerate(list_of_dict):
-        element["ID"] = db_df.index[index]
+        element["Zeilennummer"] = db_df.index[index]
 
     return db_df, list_of_dict
 
@@ -79,7 +79,8 @@ external_stylesheets = [
     },
 ]
 #start a dash instance
-# suppress_callback_exceptions=True -> the line "html.Button('Speichern', id='save_button')"
+#-----------------------
+# suppress_callback_exceptions=True -> the line "html.Button("Speichern", id="save_button")"
 # is only in the second tab, however, dash expect it to be in the initial setup
 # which would lead to the creation of the save button in every tab. The option
 # allows elements to not be loaded immediately
@@ -92,24 +93,31 @@ app.title = "PlantDB"
 app.layout = html.Div([
     html.Div(className = "header", children = [
     #primary heading
-    html.P(children = "🌿", className = "header-emoji"),
-    html.H1(children="PlantDB", className = "header-title"),
-    html.P(children = ("Open Source Pflanzen-Datenbank"), className = "header-description")
+    #html.P(children = "🌿", className = "header-emoji"),
+    html.H1(children="🌿 PlantDB", className = "header-title"),
+    html.Div([
+        html.P(children = ("Open Source Pflanzen-Datenbank"), className = "text-center"),
+        html.A("Anleitung 📃", href="/assets/Anleitung_PlantDB.pdf", target="_blank", className = "text-right")
+    ], className = "column"),
+    html.Div([
+        html.P("", className = "text-center"),
+        html.P("Created by Benno 2025", className = "text-right")
+    ], className = "column")
     ]),
     #introduce tabs
     html.Div(className = "sub-header", children = [
         dcc.Tabs(id = "tabs", value = "tab_table", children = [
-            dcc.Tab(label="Tabelle", value = "tab_table", className='tab-style', selected_className='selected-tab-style'),
-            dcc.Tab(label="Datenänderung", value = "tab_datainput", className='tab-style', selected_className='selected-tab-style'),
+            dcc.Tab(label="Tabelle", value = "tab_table", className="tab-style", selected_className="selected-tab-style"),
+            dcc.Tab(label="Datenänderung", value = "tab_datainput", className="tab-style", selected_className="selected-tab-style"),
         ]),
-        html.Div(id="database"),
-        html.Div(id="content")
+        html.Div(id = "database"),
+        html.Div(id = "content")
     ])])
 
 #controls the tabs, based on user input
 @callback(
-        Output("database", 'children'),
-        Input('tabs', 'value'))
+        Output("database", "children"),
+        Input("tabs", "value"))
 
 def render_content(tab):
     """
@@ -129,80 +137,95 @@ def render_content(tab):
     db_df, list_of_dict = loadTable(database_path = database_path)
     
     #tab 1
-    if tab == 'tab_table':
-        return html.Div(className = "sub-header", children = [
-            html.H1(children = "Pflanzentabelle", className = "sub-header"),
-            #add a paragraph
-            html.P(children = ("Datenübersicht und Filterung"),),
-            #https://dash.plotly.com/datatable
-            dash_table.DataTable( 
-                id = "datatable",
-                data = list_of_dict,
-                #add id column manually
-                columns = [{'name': 'ID', 'id': 'ID', 'presentation': 'markdown'}] + [{'name': col, 'id': col, 'presentation': 'markdown'} for col in db_df.columns],
-                filter_action = 'native',
-                style_table = {'overflowX': 'auto'},
-                style_cell = {'textAlign': 'left'},
-                style_data={'whiteSpace': 'normal', 'height': 'auto'},
-                markdown_options={'html': True}
-            )
+    if tab == "tab_table":
+        #return html.Div(className = "sub-header", children = [
+        return html.Div([
+            html.Div([
+                html.H1(children = "Datenübersicht und Filterung")
+            ], className = "sub-header"),
+            html.Div([
+                #add a paragraph
+                #html.P(children = ("Datenübersicht und Filterung"),),
+                #https://dash.plotly.com/datatable
+                #https://dash.plotly.com/datatable/style
+                dash_table.DataTable( 
+                    id = "datatable",
+                    data = list_of_dict,
+                    #add id column manually
+                    style_header = {'fontWeight': 'bold', 'backgroundColor': 'rgb(124, 124, 124)','color': 'white', "textAlign": "center", "fontSize": "18px"},
+                    columns = [{"name": "Zeilennummer", "id": "Zeilennummer", "presentation": "markdown"}] + [{"name": col, "id": col, "presentation": "markdown"} for col in db_df.columns],
+                    #one column different from the others
+                    #style_data_conditional=[{
+                    #    "if": {"column_id": "Zeilennummer"},   # 3. Zeile (Index 2)
+                    #    "fontSize": "10px",
+                    #    "padding": "2px 5px", #oben, rechts, unten, links
+                    #    "height": "50px",
+                    #    "textAlign": "center"
+                    #}],
+                    style_cell = {"minHeight": "30px", "height": "auto", "lineHeight": "15px", "textAlign": "center", 'verticalAlign': 'middle'},
+                    filter_action = "native",
+                    style_table = {"overflowX": "scroll"},
+                    style_data = {"whiteSpace": "normal", "height": "auto"},
+                    markdown_options = {"html": True}
+                )
+            ])
         ])
     #tab 2
-    elif tab == 'tab_datainput':
+    elif tab == "tab_datainput":
         return html.Div([
             #data row input GUI
             #-------------------
             html.H1("Daten ändern", className = "sub-header"),
             html.P(children = ("Zeilen anfügen oder löschen")),
-            html.H3("Zeile hinzufügen"),
-            html.P("Bitte die Daten eingeben, die hinzugefügt werden sollen. Die Spalte 'Bild_ID' ist ein Pflichtfeld."),
+            html.H3("Pflanze einsetzen"),
+            html.P('Bitte die Daten eingeben, die hinzugefügt werden sollen. Die Spalte "Bild_ID" ist ein Pflichtfeld.'),
             #list comprehension for each column in the df to dynamically depict 
             # df input fields
             html.Div([
             #on inputs: https://dash.plotly.com/dash-core-components/input
-            dcc.Input(id={"type" : "dynamic_input", "index" : df_col}, 
+            dcc.Input(id = {"type" : "dynamic_input", "index" : df_col}, 
                           type = "text", 
                           placeholder = f"{df_col}",
                           value = None)
-                for df_col in db_df.columns if df_col != "ID"
+                for df_col in db_df.columns if df_col != "Zeilennummer"
             ]),
             
             #save button
-            html.Button('Zeile hinzufügen', id = "save_button", n_clicks=0),
-            html.Div(id='output1'),
+            html.Button("Zeile hinzufügen", id = "save_button", n_clicks=0),
+            html.Div(id = "output1"),
             
             #row deletion GUI
             #----------------
-            html.H3("Zeile löschen"),
-            html.P("Bitte die ID der Zeile eingeben, die gelöscht werden soll."),
+            html.H3("Pflanze kompostieren"),
+            html.P("Bitte die Zeilennummer der Zeile eingeben, die gelöscht werden soll."),
             dcc.Input(id = "row_deletion_id", type = "text", placeholder = "Zeilen ID", value = None),
             #about buttons: https://dash.plotly.com/dash-html-components/button
-            html.Button('Zeile entfernen', id = 'save_button_row_deletion', n_clicks=0),
-            html.Div(id = 'output2')
+            html.Button("Zeile entfernen", id = "save_button_row_deletion", n_clicks=0),
+            html.Div(id = "output2")
             
             # #column add GUI
             # #---------------
             # html.H3("Spalte hinzufügen"),
             # html.P("Bitte den neu hinzuzufügenden Feldnamen angeben."),
             # dcc.Input(id = "column_addition_id", type = "text", placeholder = "Spaltenname", value = None),
-            # html.Div(id = 'output3'),
+            # html.Div(id = "output3"),
             
             # #column deletion GUI
             # #--------------------
             # html.H3("Spalte löschen"),
             # html.P("Bitte den Spaltennamen angeben, der gelöscht werden soll."),
             # dcc.Input(id = "column_deletion_id", type = "text", placeholder = "Spaltenname", value = None),
-            # html.Div(id = 'output4')
+            # html.Div(id = "output4")
             ]) 
 
 #data row input processing
 #-------------------------
 @callback(
-    Output('output1', 'children'),
-    Output('save_button', 'n_clicks'), #reset the button, everytime it was clicked by returnin 0 as a second value, reassigning it to n_clicks
-    Input({'type': 'dynamic_input', 'index': ALL}, 'value'),
-    Input("save_button", 'n_clicks')
-    #State('datatable', 'data'),
+    Output("output1", "children"),
+    Output("save_button", "n_clicks"), #reset the button, everytime it was clicked by returnin 0 as a second value, reassigning it to n_clicks
+    Input({"type": "dynamic_input", "index": ALL}, "value"),
+    Input("save_button", "n_clicks")
+    #State("datatable", "data"),
     )
 
 # def display_inputs(values, n_clicks):
@@ -227,24 +250,24 @@ def save_data(values:list, btn1_clicks:int) -> str:
     #field "Bild_ID" must not be empty
     #elif btn1_clicks > 0 and values[db_df.columns.get_loc("Bild_ID")] is None:
     #    #raise PreventUpdate
-    #    return 'Output 1: Das Feld "Bild_ID" darf nicht leers sein.', 0, no_update
+    #    return "Output 1: Das Feld "Bild_ID" darf nicht leers sein.", 0, no_update
     #at least one value must be entered
     elif btn1_clicks > 0 and values != [None for i in range(len(values))]: #and values[db_df.columns.get_loc("Bild_ID")] is not None:
         #Bild_Name input (picture name) to valid path
-        values[db_df.columns.get_loc("Bild_Name")] == f'<img src="/assets/{values[db_df.columns.get_loc("Bild_Name")]}" height="60">'
-        #'<img src="/assets/1_Gänseblümchen.jpg" height="60">'
+        values[db_df.columns.get_loc("Bild_Name")] == f'<img src="/assets/{values[db_df.columns.get_loc("Bild_Name")]}" height="100">'
+        #"<img src="/assets/1_Gänseblümchen.jpg" height="60">"
         #append list at the end end and export to csv
         db_df.loc[len(db_df)] = values
-        db_df.to_csv(database_path, index = "ID", sep = ";")
+        db_df.to_csv(database_path, index = "Zeilennummer", sep = ";")
         return "Output 1: Zeile hinzugefügt!", 0
 
 #data row deletion processing
 #-------------------------
 @app.callback(
-    Output('output2', 'children'),
-    Output('save_button_row_deletion', 'n_clicks'), #reset the button, everytime it was clicked by returnin 0 as a second value, reassigning it to n_clicks
-    Input('row_deletion_id', 'value'),
-    Input('save_button_row_deletion', 'n_clicks')
+    Output("output2", "children"),
+    Output("save_button_row_deletion", "n_clicks"), #reset the button, everytime it was clicked by returnin 0 as a second value, reassigning it to n_clicks
+    Input("row_deletion_id", "value"),
+    Input("save_button_row_deletion", "n_clicks")
     )
 
 def deleteRow(value:list, btn2_clicks:int) -> str:
@@ -267,32 +290,32 @@ def deleteRow(value:list, btn2_clicks:int) -> str:
     if btn2_clicks == 0:
         return "Output 2: Bitte Zeilen-ID eingeben, die gelöscht werden soll.", 0
     elif btn2_clicks > 0 and value is None:
-        return 'Output 2: Bitte eine valide Zahl eingeben.', 0
+        return "Output 2: Bitte eine valide Zahl eingeben.", 0
     elif btn2_clicks > 0 and not value.isdigit():
-        return 'Output 2: Bitte eine valide Zahl eingeben.', 0
+        return "Output 2: Bitte eine valide Zahl eingeben.", 0
     elif btn2_clicks > 0 and value.isdigit():
         db_df = db_df.drop(index = int(value))
-        db_df.to_csv(database_path, index = "ID", sep = ";")
-        return 'Output 2: Zeile gelöscht!', 0
+        db_df.to_csv(database_path, index = "Zeilennummer", sep = ";")
+        return "Output 2: Zeile gelöscht!", 0
 
 # #data column addition processing
 # #------------------------------
 # @app.callback(
-#     Output('output3', 'children'),
-#     Input('column_addition_id', 'value')
+#     Output("output3", "children"),
+#     Input("column_addition_id", "value")
 # )
 # def columnAddition(value):
     
-#     return f'Output 3: {value}'
+#     return f"Output 3: {value}"
 
 # #data column deletion processing
 # #--------------------------------
 # @app.callback(
-#     Output('output4', 'children'),
-#     Input('column_deletion_id', 'value')
+#     Output("output4", "children"),
+#     Input("column_deletion_id", "value")
 # )
 # def update_output_4(value):
-#     return f'Output 4: {value}'
+#     return f"Output 4: {value}"
     
 #run the app
 if __name__ == "__main__":
